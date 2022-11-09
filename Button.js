f96feaf4c7387990d8a60527577ce5089d84d958
Group055 <Group055@infoway.com>
2022-11-09 06:29:39 -0500

    added

---
+++ Button.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+  
+
+const Button = (props) => {
+  return (
+    <input
+      type="button"
+      value={props.label}
+    />
+  );
+}
+  
+
+export default Button;
